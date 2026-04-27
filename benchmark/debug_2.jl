88ce7a1327647216a4c98e54fb398266abf637ca
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2026-04-27 10:29:06 +0200

    Verified that replacing OrdinayDiffEq with DifferentialEquations made no difference in the FlexUnits example

--- benchmark/debug_2.jl
+++ benchmark/debug_2.jl
@@ -1,5 +1,6 @@
 using FlexUnits, .UnitRegistry
-using OrdinaryDiffEq
+# using OrdinaryDiffEq
+using DifferentialEquations
 using StaticArrays
 using Plots
 using BenchmarkTools
@@ -55,6 +56,6 @@ u0 = FallingObjectState(v=0.0u"m/s", h=100u"m")
 p  = FallingObjectProps(Cd=1.0u"", A=0.1u"m^2", ρ=1.0u"kg/m^3", m=50u"kg", g=9.81u"m/s^2")
 
 tspan = (0.0u"s", 10.0u"s")
-prob = ODEProblem{false, OrdinaryDiffEq.SciMLBase.NoSpecialize}(acceleration_ustatic, u0, tspan, p, abstol=[1e-6, 1e-6], reltol=[1e-6, 1e-6])
+prob = ODEProblem{false, DifferentialEquations.SciMLBase.NoSpecialize}(acceleration_ustatic, u0, tspan, p, abstol=[1e-6, 1e-6], reltol=[1e-6, 1e-6])
 sol = solve(prob, Tsit5())
 plt = plot(ustrip.(sol.t), [ustrip(u.v) for u in sol.u], label="Tsit5")
